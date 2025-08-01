41a9cda4e7fe1751df17aa250636899b9614a396
hym <yaquehym@hotmail.com>
2025-07-31 19:09:17 -0600

    feat(casos): add transaction on operation reassignament caso

--- backend/src/controllers/caso.controller.ts
+++ backend/src/controllers/caso.controller.ts
@@ -7,6 +7,7 @@ import { LogAsignacionCaso } from '../models/logAsignacionCasos.model';
 import { Usuario } from '../models/usuario.model';
 import { RESULTADO_ASIGNACION, CASO_ESTADOS } from '../constants'; 
 import { Result } from 'tedious/lib/token/helpers';
+import { sequelize } from '../config/db';
 
 export async function getCasos(req: Request, res: Response) {
     try {
@@ -122,95 +123,99 @@ export async function updateCaso(req: Request, res: Response) {
 }
 
 export async function reasignar(req: Request, res: Response) {
-    const { nuevo_fiscal_id, motivo  } = req.body;
+    const { nuevo_fiscal_id, motivo } = req.body;
     const { caso_id } = req.params;
     const { user } = req.session;
     let asignacion_result_message;
-    try{
-        const casoInfo = await Caso.findByPk(caso_id);
-        if(casoInfo == null){
-            return sendResponse(res, 404, {
-                status: 'fail',
-                message: `El caso con id:${caso_id} no fue encontrado`
-            });
-        }
+    try {
+        //inicia transacción para garantizar atomicidad de datos
+        await sequelize.transaction(async (t) => {
+            const casoInfo = await Caso.findByPk(caso_id);
+            if (casoInfo == null) {
+                return sendResponse(res, 404, {
+                    status: 'fail',
+                    message: `El caso con id:${caso_id} no fue encontrado`
+                });
+            }
 
-        const fiscalNuevoInfo = await Usuario.findByPk(nuevo_fiscal_id);
-        if(fiscalNuevoInfo == null){
-            return sendResponse(res, 404, {
-                status: 'fail',
-                message: `El nuevo_fiscal_id:${caso_id} no fue encontrado`
-            });
-        }
+            const fiscalNuevoInfo = await Usuario.findByPk(nuevo_fiscal_id);
+            if (fiscalNuevoInfo == null) {
+                return sendResponse(res, 404, {
+                    status: 'fail',
+                    message: `El nuevo_fiscal_id:${caso_id} no fue encontrado`
+                });
+            }
 
-        const allowedRoles: Role[] = [ROLES.FISCAL, ROLES.ADMIN_FISCALIA];
-        const partialInfo = {
+            const allowedRoles: Role[] = [ROLES.FISCAL, ROLES.ADMIN_FISCALIA];
+            const partialInfo = {
                 caso_id,
                 fiscal_anterior_id: casoInfo.usuario_asignado_id,
                 fiscal_nuevo_id: fiscalNuevoInfo.id,
                 motivo,
                 usuario_intento: user?.id!,
             }
-        if( !user || allowedRoles.includes( user.rol as Role)){
-            asignacion_result_message = 'No autorizado. No cuenta con los permisos necesarios';
-            await LogAsignacionCaso.create({
-                ...partialInfo,
-                resultado: RESULTADO_ASIGNACION.FALLO,
-                resultado_mensaje: asignacion_result_message,
-                fecha: new Date()
-            });
-            
-            return sendResponse(res, 403, {
-                status: 'fail',
-                message: asignacion_result_message
-            });
-        }
-        //comprobando que el caso pertenece a la misma fiscalía que el anterior
-        const fiscalAnteriorInfo = await Usuario.findByPk(casoInfo.usuario_asignado_id);
-        if(fiscalAnteriorInfo && fiscalAnteriorInfo.fiscalia_id !== fiscalNuevoInfo.fiscalia_id){
-            asignacion_result_message = 'No autorizado. Fiscal seleccionado no pertenece a la misma fiscalía que el anterior';
-            await LogAsignacionCaso.create({
-                ...partialInfo,
-                resultado: RESULTADO_ASIGNACION.FALLO,
-                resultado_mensaje: asignacion_result_message,
-                fecha: new Date()
-            });
-            
-            return sendResponse(res, 403, {
-                status: 'fail',
-                message: asignacion_result_message
-            });
-        }
+            if (!user || allowedRoles.includes(user.rol as Role)) {
+                asignacion_result_message = 'No autorizado. No cuenta con los permisos necesarios';
+                await LogAsignacionCaso.create({
+                    ...partialInfo,
+                    resultado: RESULTADO_ASIGNACION.FALLO,
+                    resultado_mensaje: asignacion_result_message,
+                    fecha: new Date()
+                });
 
-        if(CASO_ESTADOS.PENDIENTE != casoInfo.estado){
-            asignacion_result_message = 'No autorizado. El caso tiene un estado diferente a pendiente';
-            await LogAsignacionCaso.create({
-                ...partialInfo,
-                resultado: RESULTADO_ASIGNACION.FALLO,
-                resultado_mensaje: asignacion_result_message,
-                fecha: new Date()
-            });
-            
-            return sendResponse(res, 403, {
-                status: 'fail',
-                message: asignacion_result_message
-            });
-        }
+                return sendResponse(res, 403, {
+                    status: 'fail',
+                    message: asignacion_result_message
+                });
+            }
+            //comprobando que el caso pertenece a la misma fiscalía que el anterior
+            const fiscalAnteriorInfo = await Usuario.findByPk(casoInfo.usuario_asignado_id);
+            if (fiscalAnteriorInfo && fiscalAnteriorInfo.fiscalia_id !== fiscalNuevoInfo.fiscalia_id) {
+                asignacion_result_message = 'No autorizado. Fiscal seleccionado no pertenece a la misma fiscalía que el anterior';
+                await LogAsignacionCaso.create({
+                    ...partialInfo,
+                    resultado: RESULTADO_ASIGNACION.FALLO,
+                    resultado_mensaje: asignacion_result_message,
+                    fecha: new Date()
+                });
 
-        asignacion_result_message = 'Autorizado';
+                return sendResponse(res, 403, {
+                    status: 'fail',
+                    message: asignacion_result_message
+                });
+            }
+
+            if (CASO_ESTADOS.PENDIENTE != casoInfo.estado) {
+                asignacion_result_message = 'No autorizado. El caso tiene un estado diferente a pendiente';
+                await LogAsignacionCaso.create({
+                    ...partialInfo,
+                    resultado: RESULTADO_ASIGNACION.FALLO,
+                    resultado_mensaje: asignacion_result_message,
+                    fecha: new Date()
+                });
+
+                return sendResponse(res, 403, {
+                    status: 'fail',
+                    message: asignacion_result_message
+                });
+            }
+
+            asignacion_result_message = 'Autorizado';
+            casoInfo.set('usuario_asignado_id', fiscalNuevoInfo?.id);
+            casoInfo.save();
             const log = await LogAsignacionCaso.create({
                 ...partialInfo,
                 resultado: RESULTADO_ASIGNACION.EXITO,
                 resultado_mensaje: asignacion_result_message,
                 fecha: new Date()
             });
-            
+
             return sendResponse(res, 200, {
                 status: 'success',
                 message: asignacion_result_message
             });
-
-    }catch(error){
+        });
+    } catch (error) {
         console.log(error);
         return sendResponse(res, 500, {
             status: 'error',
